18b0a6a199bbc24ddfa454708e3de6600afc061d
GroovyWizard <lucasfelipe260702@gmail.com>
2022-07-10 14:36:50 -0300

    Added mutable vector to battleship

--- battleship/src/main.rs
+++ battleship/src/main.rs
@@ -27,18 +27,14 @@ fn request_warrior_name() -> String {
     input_name.trim().to_lowercase()
 }
 
-fn validate_marine(name: String) -> bool {
-    let mut space_marines_list: Vec<SpaceMarine> = vec![
-        SpaceMarine::new("noron", "space wolves"),
-        SpaceMarine::new("olaf", "space wolves"),
-        SpaceMarine::new("luciun", "black templars"),
-        SpaceMarine::new("nostrum", "space marines"),
-    ];
-    let approved_marine = &space_marines_list.iter().find(|marine| marine.name == name);
+fn validate_marine(space_marines_list: &mut Vec<SpaceMarine>, name: String) -> bool {
+    let approved_marine = space_marines_list.iter().find(|marine| marine.name == name);
 
     match approved_marine {
         Some(marine) => {
             marine.greet_marine();
+            println!("List of marines:");
+            println!("{:#?}", space_marines_list);
             return true;
         }
         None => {
@@ -46,10 +42,15 @@ fn validate_marine(name: String) -> bool {
                 println!(
                     "Get off Heretic, you are not allowed! General, he refuses to say his name!",
                 );
+                println!("List of marines:");
+                println!("{:#?}", space_marines_list);
                 return false;
             } else {
                 println!("{} is not on the marine list, but he looks rather strong and i want him in the crew, welcome {}" , name, name);
                 space_marines_list.push(SpaceMarine::new(&name, "space marine"));
+                println!("List of marines:");
+                println!("{:#?}", space_marines_list);
+
                 return true;
             }
         }
@@ -57,10 +58,17 @@ fn validate_marine(name: String) -> bool {
 }
 
 fn main() {
+    let mut space_marines_list: Vec<SpaceMarine> = vec![
+        SpaceMarine::new("noron", "space wolves"),
+        SpaceMarine::new("olaf", "space wolves"),
+        SpaceMarine::new("luciun", "black templars"),
+        SpaceMarine::new("nostrum", "space marines"),
+    ];
+
     let mut continue_asking: bool = true;
     while continue_asking {
         println!("What's your name Warrior?");
         let user_name: String = request_warrior_name();
-        continue_asking = validate_marine(user_name);
+        continue_asking = validate_marine(&mut space_marines_list, user_name);
     }
 }
